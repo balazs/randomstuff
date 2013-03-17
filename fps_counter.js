a2a6114d2c18d3d85f2ca4e55f1da83986af2b6e
Balazs Kelemen <kbalazs@webkit.org>
2013-03-17 16:27:02 +0100

    Improve fps_counter.js

--- fps_counter.js
+++ fps_counter.js
@@ -1,16 +1,50 @@
 var frameTimeStamps = new Array();
-var MAX_FRAMES = 20;
-var KEEP_FRAMES = 10;
+var WINDOW = 20;
+var SAMPLES_PER_AVG_RECOUNT = 5;
+var minFps = Infinity;
+var maxFps = -Infinity;
+var samples = new Array();
+var oldSum = 0;
+var numSamples = 0;
 
-var tick = function() {
-    frameTimeStamps.push(Date.now());
-    if (frameTimeStamps.length == MAX_FRAMES) {
-        var elapsed = frameTimeStamps[frameTimeStamps.length - 1] - frameTimeStamps[0];
-        var fps = (1000 / elapsed) * frameTimeStamps.length;
-        document.getElementById('fps_counter').innerHTML = fps;
-        // Remove oldest.
-        frameTimeStamps.splice(0, MAX_FRAMES - KEEP_FRAMES);
+var tick =
+    function()
+    {
+        var round2 = function(x)
+        {
+            var xF = Math.floor(x);
+            return xF + (Math.floor(((x - xF) * 1e2)) / 1e2);
+        }
+        var emitValue = function(id, value) { document.getElementById(id).innerHTML = round2(value); }
+
+        frameTimeStamps.push(Date.now());
+
+        // Sanity check
+        if (frameTimeStamps.length > WINDOW + 1) {
+            document.getElementById('fps_counter').innerHTML = "error";
+            return;
+        }
+
+        if (frameTimeStamps.length == WINDOW + 1) {
+            var elapsed = frameTimeStamps[frameTimeStamps.length - 1] - frameTimeStamps[0];
+            var fps = 1000 * ((frameTimeStamps.length - 1) / elapsed);
+            samples.push(fps);
+            ++numSamples;
+            emitValue('fps_counter', fps);
+            frameTimeStamps.splice(0, WINDOW / 4); // Remove some old values.
+        }
+
+        if (samples.length == SAMPLES_PER_AVG_RECOUNT) {
+            var sum = oldSum;
+            for (var i = 0; i < samples.length; ++i)
+                sum += samples[i];
+            oldSum = sum;
+            var avg = sum / numSamples;
+            samples.length = 0;
+            emitValue('avg_fps_counter', avg);
+        }
+
+        requestAnimationFrame(tick);
     }
-    requestAnimationFrame(tick);
-};
+
 requestAnimationFrame(tick);
